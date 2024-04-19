8d254a2ec7cec62a723ecbe6c0f3d0475df71669
Shivam Gupta <shivam.s.gupta1211@gmail.com>
2024-04-19 23:24:14 +0530

    commit
    commit

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ document.getElementById('button1').addEventListener('click', function () {
     handleButtonClick('https://shivo1211.github.io/game-snake/');
 });
 
-// document.getElementById('button2').addEventListener('click', function () {
-//     handleButtonClick('https://googel.com');
-// });
+document.getElementById('button2').addEventListener('click', function () {
+    handleButtonClick('https://shivo1211.github.io/rollDice/');
+});
 
 // document.getElementById('button3').addEventListener('click', function () {
 //     handleButtonClick('https://yahoo.com');
