2f2f61621cad1eda304c900db58211034a81b11c
Shivam Gupta <49141604+shivo1211@users.noreply.github.com>
2024-04-23 16:22:07 +0530

    Tic Tac Toe

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ document.getElementById('button2').addEventListener('click', function () {
     handleButtonClick('https://shivo1211.github.io/rollDice/');
 });
 
+document.getElementById('button3').addEventListener('click', function () {
+    handleButtonClick('https://shivo1211.github.io/game-XO/');
+});
+
 // document.getElementById('button3').addEventListener('click', function () {
 //     handleButtonClick('https://yahoo.com');
 // });
